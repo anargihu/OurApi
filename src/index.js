b9d8d8a488f2b1b81a8976a971a56a5f1e426c81
Anar <ndjd86d5fw@privaterelay.appleid.com>
2026-09-15 21:33:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const encoder = new TextEncoder();
 
-const WEBSITE_ORIGIN = "https://72c90004-ourwebsite.ourweb.workers.dev";
+const WEBSITE_ORIGIN = "https://ourwebsite.ourweb.workers.dev";
 
 function corsHeaders(origin) {
   const allowedOrigin = origin === WEBSITE_ORIGIN ? WEBSITE_ORIGIN : "null";
